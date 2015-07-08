7d5f248b39a324cb0fe3319d84b3cc7926e6729f
d2004 <davideramondetti@yahoo.it>
2015-07-08 13:34:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-
+<?php
+$fake_mail = $_POST['fakemail'];
+$fake_name = $_POST['fakename'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$header = "from: ".$fake_name."<$fake_mail>";
+/* Please do not remove this ad. If you remove is a copyright violation! */
+class ad {
+  echo "MailFaker by RamondettiDavide Spa - ganzonet.ramondettidavide.com - Buy PRO to Remove this Ad";
+}
+if ($_POST['submit']){
+  if ($header && $fake_mail && $email && $subject && $message){
+    $mail = mail(
+      $email,
+      $subject,
+      $message,
+      $header
+    );
+    if ($mail == true){
+      $cr = "Completed";
+    } else {
+      $cr = "False"
+    }
+  } else {
+    $cr = "Empty"
+  }
+}
+$result = array(
+  'Completed' => 'Email Inviata Con Successo',
+  'False' => 'Errore di Invio',
+  'Empty' => 'Completa Tutti I Campi'
+);
+include('fake.html');
+?>
